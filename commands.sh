0f23478da6c5869ecb2e5b726cadced1f5e85476
Andrew Kiggins <30563055+Kiggins26@users.noreply.github.com>
2020-11-09 19:42:12 -0800

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -2,3 +2,8 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cd .vim/plugged/youcompleteme/
 python3 install.py --all
+
+mkdir -p ~/.vim/pack/tpope/start
+cd ~/.vim/pack/tpope/start
+git clone https://tpope.io/vim/surround.git
+vim -u NONE -c "helptags surround/doc" -c q
